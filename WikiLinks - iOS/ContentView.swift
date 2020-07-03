627bc6342e5ffece0be4f2146f888e2301835afc
Anton Styagun <astyagun@ya.ru>
2020-07-03 15:43:36 +0300

    Initial Commit

---
+++ WikiLinks - iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WikiLinks - iOS
+//
+//  Created by Anton Styagun on 03.07.2020.
+//  Copyright © 2020 Anton Styagun. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
